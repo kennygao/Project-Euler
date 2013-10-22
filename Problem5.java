c414d7c444328df3ca6645c0949ad95652a6376d
kg <kennytgao@gmail.com>
2013-10-22 00:25:20 -0700

    Problem5.java

---
+++ Problem5.java
@@ -0,0 +1,45 @@
+/**
+ * Project Euler Problem 5
+ */
+package me.phra.projecteuler;
+
+/**
+ * @author kennygao
+ */
+public class Problem5 {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        int lower = 1;
+        int upper = 20;
+        long candidate = 1;
+
+        // start with the product of the divisors as an upper bound for the answer
+        for (int i = lower; i <= upper; i++) {
+            candidate *= i;
+        }
+
+        // "pare down" the answer
+        for (int i = lower; i <= upper; i++) {
+            if (i == 1) continue;
+
+            while (isDivisible(candidate / i, lower, upper)) {
+                candidate /= i;
+            }
+        }
+
+        System.out.println(candidate);
+    }
+
+    private static boolean isDivisible(long n, int lower, int upper) {
+        for (int i = lower; i <= upper; i++) {
+            if (n % i != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
