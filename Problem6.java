7d3da5afe5117df7dcc276d9dbc4e7cac069ce71
kg <kennytgao@gmail.com>
2014-03-29 13:06:56 -0700

    Problem6.java - added stream implementation.

--- Problem6.java
+++ Problem6.java
@@ -3,6 +3,8 @@
  */
 package me.phra.projecteuler;
 
+import java.util.stream.IntStream;
+
 /**
  * @author kennygao
  */
@@ -12,6 +14,11 @@ public class Problem6 {
      * @param args
      */
     public static void main(String[] args) {
+        explicit();
+        stream();
+    }
+
+    private static void explicit() {
         int sumOfSquares = 0;
         int squareOfSums = 0;
 
@@ -20,6 +27,14 @@ public class Problem6 {
             squareOfSums += i;
         }
         squareOfSums *= squareOfSums;
+
+        System.out.println(squareOfSums - sumOfSquares);
+    }
+
+    private static void stream() {
+        int sumOfSquares = IntStream.rangeClosed(1, 100).map(i -> i * i).sum();
+        int sum = IntStream.rangeClosed(1, 100).sum();
+        int squareOfSums = sum * sum;
 
         System.out.println(squareOfSums - sumOfSquares);
     }
